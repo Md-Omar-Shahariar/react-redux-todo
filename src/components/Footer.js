ae6690c468c8313beaeac1817b5c62cdd6c27dba
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-12-30 14:51:20 +0600

    filtercolor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { statusChanged } from "../redux/filter/action";
+import { colorChanged, statusChanged } from "../redux/filter/action";
 
 const Footer = () => {
   const todos = useSelector((state) => state.todos);
@@ -10,6 +10,13 @@ const Footer = () => {
   const todosRemaining = todos.filter((todo) => !todo.completed).length;
   const handleStatusChanged = (status) => {
     dispatch(statusChanged(status));
+  };
+  const handleColorChange = (color) => {
+    if (colors.includes(color)) {
+      dispatch(colorChanged(color, "remove"));
+    } else {
+      dispatch(colorChanged(color, "added"));
+    }
   };
   return (
     <div class="mt-4 flex justify-between text-xs text-gray-500">
@@ -44,9 +51,24 @@ const Footer = () => {
         </li>
         <li></li>
         <li></li>
-        <li class="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
-        <li class="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"></li>
-        <li class="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"></li>
+        <li
+          onClick={() => handleColorChange("green")}
+          class={`h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer ${
+            colors.includes("green") && "bg-green-500"
+          }`}
+        ></li>
+        <li
+          onClick={() => handleColorChange("red")}
+          class={`h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer ${
+            colors.includes("red") && "bg-red-500"
+          }`}
+        ></li>
+        <li
+          onClick={() => handleColorChange("yellow")}
+          class={`h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer ${
+            colors.includes("yellow") && "bg-yellow-500"
+          }`}
+        ></li>
       </ul>
     </div>
   );
