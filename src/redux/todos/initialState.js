4576338670f9e2e4a184c0986967e50165efd59b
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-02 23:52:22 +0600

    init

--- src/redux/todos/initialState.js
+++ src/redux/todos/initialState.js
@@ -1,13 +1,1 @@
-export const initialState = [
-  {
-    id: 1,
-    text: "Learn React JS",
-    completed: false,
-  },
-  {
-    id: 2,
-    text: "Learn Redux",
-    completed: false,
-    color: "red",
-  },
-];
+export const initialState = [];
